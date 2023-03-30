cb81faa129f7b2597e653896df08983c14798de2
Vicente Cabezas <1977259@claurendeau.qc.ca>
2023-03-29 21:40:51 -0400

    message roberto

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 
 NE MODIFIEZ PAS CE FICHIER (sauf pour mettre certaines sections en commentaire le temps de tester votre code) */
 
+//ROBERTO EST-CE QUE TU VOIS CECI?:
+
 public class Main {
     public static void main(String[] args) {
         commencerTest("Ajout de mots et de lettres");
